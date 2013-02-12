cb9dc68fff63496096eab4d059434584bc2bbefa
David Pilato <david@pilato.fr>
2013-02-12 10:03:52 +0100

    Update demo

--- src/main/java/org/elasticsearchfr/jug/LaunchNode.java
+++ src/main/java/org/elasticsearchfr/jug/LaunchNode.java
@@ -1,19 +1,20 @@
 package org.elasticsearchfr.jug;
 
-import java.io.File;
-import java.io.IOException;
-
+import org.apache.log4j.Logger;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.common.io.FileSystemUtils;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeBuilder;
+
+import java.io.File;
+import java.io.IOException;
 
 public class LaunchNode {
+    private static final Logger logger = Logger.getLogger(LaunchNode.class);
+
 	protected static Node node;
 
 	/**
@@ -27,16 +28,32 @@ public class LaunchNode {
 		removeOldDataDir();
 		
 		// Let's generate 10 nodes (ignore it for the first time)
-		
-		// Create a client Node
+        for (int i = 0; i < 10; i++) {
+            // TODO Add nodes
+        }
+
+        // TODO Create a client Node
 		node = null;
-		
-		// Wait for healthy status
-		
-		// Let's index 10000 docs
+
+		// TODO Wait for healthy status
+
+        logger.info("Start indexing");
+
+        // TODO Prepare a Bulk
+        BulkRequestBuilder brb = null;
+
+        // TODO Let's index 10000 docs
+        for (int i = 0; i < 10000; i++) {
+            // TODO Add index operation to bulk
+         }
+
+        // TODO Execute the bulk
+
+        logger.info("End of indexing");
 
 		// Wait 10 minutes
-		Thread.sleep(10 * 60 * 1000);
+        // TODO Uncomment when using 10 embedded nodes
+		// Thread.sleep(10 * 60 * 1000);
 	}
 	
 	private static XContentBuilder jsonGenerator(int i, int j) {
